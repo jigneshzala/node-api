a6f299ff99d038d469aaafaf06ac6462a6f65976
jignesh zala <jignesh.zala@indianic.com>
2023-03-30 18:42:34 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+// Dummy data
+const users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Doe' },
+    { id: 3, name: 'Bob Smith' },
+];
+
+// Define a GET endpoint that returns the dummy data
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
